56d72530d090f466c426db91f194a595c208c18a
Kevin Ho <kevin.ho01@sap.com>
2024-03-19 01:49:33 -0700

    exammple ServiceEntryDetail via selector.js and React component

--- src/custom-ui/serviceentries/details/ServiceEntryDetail.js
+++ src/custom-ui/serviceentries/details/ServiceEntryDetail.js
@@ -1,5 +1,44 @@
+import { getCustomResource } from '../../helper';
+import '@ui5/webcomponents/dist/Badge.js';
+
 const ServiceEntryDetails = () => {
-  return <p>ServiceEntryDetails</p>;
+  const customResource = getCustomResource();
+
+  const addressList = customResource?.data?.spec?.addresses?.map(addr => {
+    return <ui5-li>{addr}</ui5-li>;
+  });
+
+  const hosts = customResource?.data?.spec?.hosts?.map(x => {
+    return <ui5-li>{x}</ui5-li>;
+  });
+
+  return (
+    <div style={{ color: 'white' }}>
+      <p>ServiceEntryDetails</p>
+      <div>
+        Location:{' '}
+        <ui5-badge color-scheme="6">
+          {customResource?.data?.spec?.location}
+        </ui5-badge>
+      </div>
+      <div>
+        Resolution:{' '}
+        <ui5-badge color-scheme="5">
+          {customResource?.data?.spec?.resolution}
+        </ui5-badge>
+      </div>
+
+      <div>
+        Addresses
+        <ui5-list data-sap-ui-fastnavgroup="true">{addressList}</ui5-list>
+      </div>
+
+      <div>
+        Hosts
+        <ui5-list data-sap-ui-fastnavgroup="true">{hosts}</ui5-list>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceEntryDetails;
